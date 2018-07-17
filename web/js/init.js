daf5b0499060b5d5ae0c64c033d055448628a2b1
Amarelle <ewige.qual@gmail.com>
2018-07-17 14:22:32 -0500

    init add

--- web/js/init.js
+++ web/js/init.js
@@ -1,18 +1,19 @@
-(function ($) {
-    $(function () {
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
 
-        $('.sidenav').sidenav();
-        $('.modal').modal();
-        $('.slider').slider();
-        $('.collapsible').collapsible();
-        $('.fixed-action-btn').floatingActionButton();
-        $('.tooltipped').tooltip();
-        $('.materialboxed').materialbox();
-        $('.tabs').tabs();
-        $('select').formSelect();
-        $('.carousel').carousel();
-        $('.carousel.carousel-slider').carousel();
+(function($){
+  $(function(){
 
-    }); // end of document ready
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+    $('select').formSelect();
+
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
-
